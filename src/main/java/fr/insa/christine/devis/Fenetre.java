ba9248cccb21507638c0653820d4e09956590b53
Valentin Billa <valentin.billa@gmail.com>
2024-05-01 15:46:21 +0200

    :building_construction: Rangement du projet
    - Ajout d'un gitignore
    - Suppression des fichiers '/target'
    - Désimbrication du projet
    - Renommage du projet en 'fr.insa.christine.devis'

---
+++ src/main/java/fr/insa/christine/devis/Fenetre.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.christine.devis;
+
+/**
+ *
+ * @author cesse
+ */
+public class Fenetre extends Ouverture {
+    
+    public Fenetre(int idOuverture, double dimX, double dimY) {
+        super(idOuverture, dimX, dimY);
+    }
+    
+}
